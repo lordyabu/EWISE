f97658aa8cd8d4b0611d49c109185f9843244f99
lordyabu <ay13@illinois.edu>
2024-01-09 17:02:14 -0600

    Springer done, but need to be fully checked

---
+++ src/springer/springer_runner.py
@@ -0,0 +1,123 @@
+from web_scraper_springer import get_latest_volume_number_springer, get_num_issues_springer, get_paper_number_from_name_springer, get_papers_link_springer, get_abstract_info_springer
+from tqdm import tqdm
+import json
+import os.path
+from config import USER_PATH, DATA_PATH
+
+
+def scrape_multiple_springer_journals(names, num_prev_vols, wait_time):
+    for name in names:
+        try:
+            automatic_scrape_springer_journal(name, num_prev_vols, wait_time)
+        except Exception as e:
+            print(e)
+
+
+def scrape_springer_journal(name, automatic_collection, num_prev_vols, manual_vols=-1, manual_issues=-1, wait_time=15):
+    if automatic_collection:
+        automatic_scrape_springer_journal(name, num_prev_vols, wait_time)
+    else:
+        manual_scrape_springer_journals(name, manual_vols, manual_issues, wait_time)
+
+
+def automatic_scrape_springer_journal(name, num_prev_vols, wait_time):
+    int_paper = get_paper_number_from_name_springer(name)
+    volume_url = f"https://link.springer.com/journal/{int_paper}"
+    journal_url = "https://link.springer.com/journal/{}/volumes-and-issues/{{}}-{{}}".format(int_paper)
+    output_path = os.path.join(DATA_PATH, f'springer_{name}.json')
+
+    html_list = []
+    abstract_list = []
+    url = []
+
+    num_issues = get_num_issues_springer(name)
+    issues = [i for i in range(1, num_issues + 1)]
+    latest_vol = get_latest_volume_number_springer(volume_url, wait_time)
+    starting_vol = max(1, latest_vol - num_prev_vols + 1)
+    volumes = [vol for vol in range(starting_vol, latest_vol + 1)]
+
+    # Generate URLs
+    try:
+        for volume in volumes:
+            if issues:
+                for issue in issues:
+                    url.append(journal_url.format(volume, issue))
+            else:
+                url.append(journal_url.format(volume))
+    except Exception as e:
+        raise RuntimeError(f"URL generation failed: {e}")
+
+    # Get links for each paper
+    for site in tqdm(url, desc="Getting paper links"):
+        try:
+            html_list = get_papers_link_springer(site, html_list, wait_time)
+            # #ToDo remove
+            # if len(html_list) > 3:
+            #     break
+        except Exception as e:
+            raise RuntimeError(f"Failed to get links for each paper: {e}")
+
+    # Get Abstracts with progress bar
+    for i in tqdm(range(len(html_list)), desc="Getting abstracts"):
+        try:
+            abstract = get_abstract_info_springer(url_paper_list=html_list, paper_number=i, wait_time=wait_time)
+            if abstract:
+                abstract_list.append(abstract)
+                # # ToDo remove
+                # break
+        except Exception as e:
+            pass
+
+    # Write data to JSON file
+    with open(output_path, 'w') as json_file:
+        json.dump(abstract_list, json_file)
+
+
+def manual_scrape_springer_journals(name, volumes, issues, wait_time):
+    int_paper = get_paper_number_from_name_springer(name)
+    journal_url = "https://link.springer.com/journal/{}/volumes-and-issues/{{}}-{{}}".format(int_paper)
+    output_path = os.path.join(DATA_PATH, f'springer_{name}.json')
+
+    html_list = []
+    abstract_list = []
+    url = []
+
+    # Generate URLs
+    try:
+        for volume in volumes:
+            if issues:
+                for issue in issues:
+                    url.append(journal_url.format(volume, issue))
+            else:
+                url.append(journal_url.format(volume))
+    except Exception as e:
+        raise RuntimeError(f"URL generation failed: {e}")
+
+    # Get links for each paper
+    for site in tqdm(url, desc="Getting paper links"):
+        try:
+            html_list = get_papers_link_springer(site, html_list, wait_time)
+        except Exception as e:
+            raise RuntimeError(f"Failed to get links for each paper: {e}")
+
+    # Get Abstracts with progress bar
+    for i in tqdm(range(len(html_list)), desc="Getting abstracts"):
+        try:
+            abstract = get_abstract_info_springer(url_paper_list=html_list, paper_number=i, wait_time=wait_time)
+            if abstract:
+                abstract_list.append(abstract)
+        except Exception as e:
+            pass
+
+    # Write data to JSON file
+    with open(output_path, 'w') as json_file:
+        json.dump(abstract_list, json_file)
+
+
+def main():
+    journal_list = ['IMF Economic Review', 'Journal of Population Economics']
+
+    scrape_multiple_springer_journals(journal_list, 2, 15)
+
+if __name__ == "__main__":
+    main()
---
+++ src/springer/web_scraper_springer.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+
+"""
+Web Scraper for Academic Journals - springer Module
+
+This module provides a web scraping tool to extract data from academic journals published by springer.
+It automates the process of accessing journal webpages and collecting information such as article titles,
+authors, abstracts, and issue/volume details using Python, Selenium, and the Firefox web driver.
+The tool is designed to assist in gathering data for academic research and analysis.
+
+Functions:
+    get_papers_link_springer(url, html_list, wait_time): Retrieves URLs of papers from a specified springer journal webpage.
+    get_abstract_info_springer(url_paper_list, paper_number, wait_time): Extracts detailed information from a springer paper's webpage,
+        including abstract, title, authors, and issue/volume information.
+
+Usage:
+    1. Collect paper URLs:
+        paper_urls = get_papers_link_springer(journal_url, [], wait_time)
+
+    2. Extract paper details:
+        paper_info = get_abstract_info_springer(paper_urls, paper_index, wait_time)
+"""
+
+# =============================================================================
+# Packages
+# =============================================================================
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from config import GECKO_PATH
+import re
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+
+#DONE
+def get_latest_volume_number_springer(url, wait_time):
+    """
+    Retrieves the latest volume number from the specified Springer journal webpage.
+
+    Args:
+        url (str): URL of the journal's webpage.
+        wait_time (int): Time to wait for page rendering before scraping.
+
+    Returns:
+        int: The latest volume number as an integer.
+    """
+    volume_number = 0
+    try:
+        service = Service(GECKO_PATH)
+        browser = webdriver.Firefox(service=service)
+        browser.get(url)
+
+        # Wait for the page to render
+        time.sleep(wait_time)
+
+        # Find the volume element and extract the number
+        volume_element = browser.find_element(By.CSS_SELECTOR, "p.app-journal-latest-issue__volume a")
+        volume_text = volume_element.text
+        match = re.search(r"Volume (\d+), Issue \d+", volume_text)
+        if match:
+            volume_number = int(match.group(1))
+
+    except Exception as e:
+        print("Error: " + str(e))
+
+    finally:
+        browser.close()
+
+    return volume_number
+
+#DONE
+def get_paper_number_from_name_springer(name):
+    with open('springer_journal_name_to_int_and_num_issues.json', 'r') as file:
+        name_dict = json.load(file)
+
+    try:
+        return name_dict[name][0]
+    except KeyError:
+        print(f"The journal name: {name} either is not a springer journal or has not been added to name->int/#issues dict.")
+
+
+#DONE
+def get_num_issues_springer(name):
+    with open('springer_journal_name_to_int_and_num_issues.json', 'r') as file:
+        name_dict = json.load(file)
+
+    try:
+        return name_dict[name][1]
+    except KeyError:
+        print(f"The journal name: {name} either is not a springer journal or has not been added to name->int/#issues dict.")
+
+
+#DONE
+def get_papers_link_springer(url, html_list, wait_time):
+    """
+    Retrieves URLs of papers from a specified Springer journal webpage.
+
+    Args:
+        url (str): URL of the journal's webpage.
+        wait_time (int): Time to wait for page rendering before scraping.
+
+    Returns:
+        paper_links (list): List of URLs of papers.
+    """
+    try:
+        service = Service(GECKO_PATH)
+        browser = webdriver.Firefox(service=service)
+        browser.get(url)
+
+        # Wait for the page to render
+        time.sleep(wait_time)
+
+        # Find all elements that match the desired selector
+        articles = browser.find_elements(By.CSS_SELECTOR, "article.c-card-open h3.c-card-open__heading a")
+
+        for article in articles:
+            link = article.get_attribute('href')
+            html_list.append(link)
+
+    except Exception as e:
+        print("Error: " + str(e))
+
+    finally:
+        browser.close()
+
+    return html_list
+
+#DONE
+def get_abstract_info_springer(url_paper_list, paper_number, wait_time):
+    """
+    Retrieves detailed information of a specific paper from Springer.
+
+    Args:
+        url_paper_list (list): List of paper URLs.
+        paper_number (int): Index of the paper in the list.
+        wait_time (int): Time to wait for page rendering before scraping.
+
+    Returns:
+        paper (list): A list containing detailed information of the paper.
+    """
+    paper = []
+    try:
+        service = Service(GECKO_PATH)
+        browser = webdriver.Firefox(service=service)
+        browser.get(url_paper_list[paper_number])
+
+        WebDriverWait(browser, wait_time).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "h1.c-article-title"))
+        )
+
+        # Find the title
+        title = browser.find_element(By.CSS_SELECTOR, 'h1.c-article-title').text
+
+        # Find the authors
+        authors_elements = browser.find_elements(By.CSS_SELECTOR, "ul.c-article-author-list li a[data-test='author-name']")
+        authors = ", ".join([author.text for author in authors_elements])
+
+        # Find the abstract
+        abstract = browser.find_element(By.CSS_SELECTOR, 'div.c-article-section__content p').text
+
+        # Find the volume and set issue to 'X'
+        volume_element = browser.find_element(By.CSS_SELECTOR, 'span[data-test="journal-volume"]')
+        volume = volume_element.text.replace("Volume", "").strip()
+        issue = "X"  # Page does not show issue
+
+        paper = [f"Volume {volume}, Issue {issue}", title, authors, abstract]
+    except Exception as e:
+        print("Error: " + str(e))
+
+    finally:
+        browser.close()
+
+    return paper
+
